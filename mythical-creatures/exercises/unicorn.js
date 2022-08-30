c7fb347d8dd318d1642638d2da77d3b315452d94
Evan Swanson <evan.sundelius@gmail.com>
2022-08-29 20:44:24 -0500

    Initial commit

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,7 +2,24 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+
+    if (color == undefined) {
+      this.color = "white";
+    }
+  }
+
+  isWhite() {
+    if (this.color == "white") {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  says(phrase) {
+    return `**;* ${phrase} *;**`
   }
 }
+
 
 module.exports = Unicorn;
