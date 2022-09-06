3df6880cfc6a69e1334eb04914a2f886d69ffca5
Evan Swanson <evan.sundelius@gmail.com>
2022-09-06 18:16:02 -0500

    Complete dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+var amount = 0
 
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
+
+greet() {
+  return `Hi, ${this.rider}!`
+}
+
+eat() {
+  amount = amount + 1
+  if (amount > 2) {
+    this.hungry = false;
+  }
+}
+}
+
+
+module.exports = Dragon;
